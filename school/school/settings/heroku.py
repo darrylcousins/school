479b496fffacc1b8cf5c524feafcbff083526729
cousinsd-ellesmere <cousinsd@ellesmere.school.nz>
2018-02-26 20:42:02 +1300

    sigh

--- school/school/settings/heroku.py
+++ school/school/settings/heroku.py
@@ -19,19 +19,5 @@ ALLOWED_HOSTS = [
 ########## END SITE CONFIGURATION
 
 ########## STATIC FILE CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
-
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = '/static/'
-
-# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = (
-    normpath(join(SITE_ROOT, 'static')),
-)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = normpath(join(SITE_ROOT, 'staticfiles'))
 ########## END STATIC FILE CONFIGURATION
