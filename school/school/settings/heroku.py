57172cf5c0ceb1216e264dc76dcc42eea12746af
cousinsd-ellesmere <cousinsd@ellesmere.school.nz>
2018-02-26 20:03:32 +1300

    can only sigh

--- school/school/settings/heroku.py
+++ school/school/settings/heroku.py
@@ -2,10 +2,10 @@ __author__ = 'Darryl Cousins <darryljcousins@gmail.com>'
 
 """Heroku settings and globals."""
 
-from os import environ
-
 from .production import *
 
-GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
+import os
+
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 GDAL_LIBRARY_PATH = os.path.join(os.environ['GDAL_LIBRARY_PATH'], 'libgdal.so')
 
