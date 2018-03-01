aa80bc7dcaea91adfb570b6a565311642e6fd056
cousinsd-ellesmere <cousinsd@ellesmere.school.nz>
2018-03-01 20:45:23 +1300

    damn heroku gdal

--- school/school/settings/heroku.py
+++ school/school/settings/heroku.py
@@ -6,8 +6,8 @@ from .production import *
 
 import os
 
-GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so'
+GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so'
 
 ########## SITE CONFIGURATION
 # Hosts/domain names that are valid for this site
